df0d2e83a8933115faeb2f0827fc6caef86dd6a7
Jakub Nenczak <kubanenczak@gmail.com>
2024-01-31 17:46:23 +0100

    added running script

--- install_zsh.sh
+++ install_zsh.sh
@@ -3,6 +3,6 @@
 sudo mkdir -p ~/.local/bin
 docker build -t "valgrind:1.0" .
 sudo chmod +x run_valgrind.sh
-sudo mv run_valgrind.sh ~/.local/bin
+sudo cp run_valgrind.sh ~/.local/bin
 echo "alias run_valgrind='~/.local/bin/run_valgrind.sh'" >> ~/.zshrc
 source ~/.zshrc
