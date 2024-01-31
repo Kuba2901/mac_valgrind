447e9b5c6de5e158008cc4eb1890a61c22612a6c
Jakub Nenczak <kubanenczak@gmail.com>
2024-01-31 16:18:59 +0100

    repushed

--- experimental/install_zsh.sh
+++ experimental/install_zsh.sh
@@ -2,7 +2,7 @@
 
 sudo mkdir ~/.local/bin
 docker build -t "valgrind:1.0" .
-chmod +x run_valgrind.sh
-mv run_valgrind.sh ~/.local/bin
+sudo chmod +x run_valgrind.sh
+sudo mv run_valgrind.sh ~/.local/bin
 echo "alias run_valgrind='~/.local/bin/run_valgrind.sh'" >> ~/.zshrc
 source ~/.zshrc
--- run_valgrind.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run -it -v $PWD:/tmp -w /tmp valgrind:1.0
